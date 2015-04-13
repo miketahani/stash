9b466c63653ab1b4c2af3a1c153ddaf0a4315123
miketahani <me@miketahani.com>
2015-04-12 18:15:15 -0700

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+### stash
+super simple tile-caching middleware. send tile URLs via GET and it will retrieve the tile and also cache it.
+
+#### why?
+this is part of a bigger project that i'm working on, and i needed a simple way to cache tiles without pre-configuring the middleware with tile URLs.
+
---
+++ stash.js
@@ -0,0 +1,60 @@
+/* 
+   stash tiles while you're exploring them in a map.
+   on a frontend map, just prefix your tile layer URL with 'http://localhost:7357/stash/' and 
+   requests will get routed through this lil server, which will save the tiles in standard slippy-format.
+   (tile URLs look like `http://example.com/{layer}/{z}/{x}/{y}.png`)
+*/
+
+var CACHE_DIR = './tile-cache/',
+    PORT = 7357;
+
+var fs = require('fs'),
+    express = require('express'),
+    request = require('request'),
+    mkdirp = require('mkdirp');
+
+var app = express();
+
+// function getTile (req, tileUrl, dir, tilePath, callback) {}
+
+app.get('/stash/:href(*)', function (req, res) {
+  
+  // parse out parts of the url to make a request
+  var tileUrl = req.params.href;
+  var parsed = tileUrl
+    .match(/(.+)\/(\w+)\/(\d+)\/(\d+)\/(\d+)\.(\w+)$/)
+    .slice(1, 7);
+
+  var dir = CACHE_DIR + parsed.slice(1, 4).join('/'),
+      tileFile = parsed[4] + '.' + parsed[5],
+      tilePath = dir + '/' + tileFile;
+
+  // check if the tile exists, else, grab it
+  fs.stat(tilePath, function (err, stats) { 
+    
+    if (!err && stats.isFile()) { 
+      console.log('[*] tile in cache: ' + tileFile);
+      fs.createReadStream(tilePath).pipe(res);
+    } else { 
+      console.log('[*] tile not in cache: ' + tileFile);
+      // create the directory structure if it doesn't exist    
+      mkdirp(dir, function (err) {
+        if (err) { console.log('[x] ERROR creating directory: ' + dir); }
+        // request the remote tile
+        console.log('[*] getting tile: ' + tileUrl);
+        var tile = request(tileUrl);
+        // FIXME is this async?
+        tile.pipe(fs.createWriteStream(tilePath));
+        tile.pipe(res);
+      });
+    
+    }
+  });
+  
+});
+
+var server = app.listen(PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('[*] app started on %s:%s', host, port);
+});
