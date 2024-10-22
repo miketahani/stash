6cbf9ca5767782ad6d43b8ad06901790912ccbb6
Mike Tahani <me@miketahani.com>
2024-10-21 21:01:38 -0700

    minor update

--- stash.js
+++ stash.js
@@ -1,60 +1,57 @@
-/* 
+/*
    stash tiles while you're exploring them in a map.
-   on a frontend map, just prefix your tile layer URL with 'http://localhost:7357/stash/' and 
+   on a frontend map, just prefix your tile layer URL with 'http://localhost:7357/stash/' and
    requests will get routed through this lil server, which will save the tiles in standard slippy-format.
    (tile URLs look like `http://example.com/{layer}/{z}/{x}/{y}.png`)
 */
+import fs from 'fs';
+import express from 'express';
+import request from 'request';
+import mkdirp from 'mkdirp';
+import cors from 'cors'
 
-var CACHE_DIR = './tile-cache/',
-    PORT = 7357;
+const CACHE_DIR = './tile-cache/';
+const PORT = 7357;
 
-var fs = require('fs'),
-    express = require('express'),
-    request = require('request'),
-    mkdirp = require('mkdirp');
-
-var app = express();
-
-// function getTile (req, tileUrl, dir, tilePath, callback) {}
+const app = express();
+app.use(cors())
 
 app.get('/stash/:href(*)', function (req, res) {
-  
+
   // parse out parts of the url to make a request
-  var tileUrl = req.params.href;
-  var parsed = tileUrl
+  const tileUrl = req.params.href;
+  const parsed = tileUrl
     .match(/(.+)\/(\w+)\/(\d+)\/(\d+)\/([\d@x]+)\.(\w+)/)
     .slice(1, 7);
 
-  var dir = CACHE_DIR + parsed.slice(1, 4).join('/'),
-      tileFile = parsed[4] + '.' + parsed[5],
-      tilePath = dir + '/' + tileFile;
+  const dir = CACHE_DIR + parsed.slice(1, 4).join('/')
+  const tileFile = parsed[4] + '.' + parsed[5]
+  const tilePath = dir + '/' + tileFile
 
   // check if the tile exists, else, grab it
-  fs.stat(tilePath, function (err, stats) { 
-    
-    if (!err && stats.isFile()) { 
+  fs.stat(tilePath, function (err, stats) {
+
+    if (!err && stats.isFile()) {
       console.log('[*] tile in cache: ' + tileFile);
       fs.createReadStream(tilePath).pipe(res);
-    } else { 
-      console.log('[*] tile not in cache: ' + tileFile);
-      // create the directory structure if it doesn't exist    
+    } else {
+      // create the directory structure if it doesn't exist
       mkdirp(dir, function (err) {
         if (err) { console.log('[x] ERROR creating directory: ' + dir); }
         // request the remote tile
         console.log('[*] getting tile: ' + tileUrl);
-        var tile = request(tileUrl);
-        // FIXME is this async?
+        const tile = request(tileUrl);
         tile.pipe(fs.createWriteStream(tilePath));
         tile.pipe(res);
       });
-    
+
     }
   });
-  
+
 });
 
-var server = app.listen(PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(PORT, function () {
+  const host = server.address().address;
+  const port = server.address().port;
   console.log('[*] app started on %s:%s', host, port);
 });
